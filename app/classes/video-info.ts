cba648cbd02b29e6a940b9173285b45664361353
Yuto Otaguro <yotagur@gmail.com>
2017-07-10 22:00:45 -0500

    View start and end code.

--- app/classes/video-info.ts
+++ app/classes/video-info.ts
@@ -1,5 +1,8 @@
 abstract class VideoPage {
     protected webHostName: string;
+    protected startTime: Date;
+    protected endTime: Date;
+    protected videoIsPlaying: boolean
 
     constructor(hostName: string) {
         this.webHostName = hostName;
@@ -29,6 +32,20 @@ abstract class VideoPage {
      * Get the current time of the video if applicable from the current series page.
      */
     abstract get currentTimeInSeconds(): number;
+    /**
+     * Call this function when the user begins viewing a video.
+     */
+    startViewing(): void {
+        this.videoIsPlaying = true;
+        this.startTime = new Date(Date.now());
+    }
+    /**
+     * Call this function when the user ends viewing a video.
+     */
+    endViewing(): void {
+        this.videoIsPlaying = false;
+        this.endTime = new Date(Date.now());
+    }
 }
 
 export default VideoPage;
