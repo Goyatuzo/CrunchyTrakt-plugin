847f2a9efc4cb8cb164d40117995116bdaa6cb1a
Yuto Otaguro <yotagur@gmail.com>
2018-01-04 23:44:25 -0600

    React's document state is always ready, so that can't be used for validity.

--- app/event-handlers/vrv-events.ts
+++ app/event-handlers/vrv-events.ts
@@ -5,37 +5,16 @@ import { IVideoData } from "../classes/video-info";
 let vrv = new Vrv();
 
 chrome.runtime.onMessage.addListener((message: IChromeMessage, sender, sendResponse) => {
+    console.log("MESSAGE RECEIVED");
     switch (message.type) {
         case ChromeMessageType.GET_VIDEO_DATA: {
 
-            if (document.readyState === "complete") {
-                sendResponse(vrv.outputVideoData());
-            }
+            sendResponse(vrv.outputVideoData());
 
-            document.onreadystatechange = () => {
-                if (document.readyState === "complete") {
-                    sendResponse(vrv.outputVideoData());
-
-                    document.onreadystatechange = null;
-                }
-            }
-            console.log(vrv.outputVideoData());
             break;
         }
 
         case ChromeMessageType.GET_CURRENT_TIMESTAMP: {
-            if (document.readyState === "complete") {
-                sendResponse(vrv.currentTimeInSeconds);
-            }
-
-            document.onreadystatechange = () => {
-                if (document.readyState === "complete") {
-                    sendResponse(vrv.currentTimeInSeconds);
-
-                    document.onreadystatechange = null;
-                }
-            }
-
             break;
         }
     }
