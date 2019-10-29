592ae404d5189314b7f54f6f336db67ceb70b38d
Yuto Otaguro <yotagur@gmail.com>
2019-10-29 02:09:08 -0500

    Response is stored in browser storage

--- app/storage/index.ts
+++ app/storage/index.ts
@@ -1,22 +1,14 @@
 import { browser } from "webextension-polyfill-ts";
 
 export class StorageWrapper {
-    private hasSync: boolean = browser.storage.sync !== null && browser.storage.sync !== undefined;
+    // private hasSync: boolean = browser.storage.sync !== null && browser.storage.sync !== undefined;
 
     get<T = any>(key: string): Promise<{ [s: string]: T }> {
-        if (this.hasSync) {
-            return browser.storage.sync.get(key);
-        } else {
-            return browser.storage.local.get(key);
-        }
+        return browser.storage.local.get(key);
     }
 
     set(key: string, value: any): Promise<void> {
-        if (this.hasSync) {
-            return browser.storage.sync.set({ key, value });
-        } else {
-            return browser.storage.local.set({ key, value });
-        }
+        return browser.storage.local.set({ [key]: value });
     }
 }
 
