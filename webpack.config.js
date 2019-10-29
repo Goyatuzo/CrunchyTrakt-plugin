66d7721a2773093366a87519130bdf0130610816
Yuto Otaguro <yotagur@gmail.com>
2019-10-29 16:30:44 -0500

    Popup properly loads up

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
 var path = require('path');
 var webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 let API_ROOT = 'https://api-staging.trakt.tv'
 
 if (process.env.NODE_ENV === 'production') {
     API_ROOT = 'https://api.trakt.tv';
 }
+
+const outputLocation = {
+    filename: "[name].js",
+    path: path.join(__dirname, "app/dist")
+};
 
 module.exports = [
     {
@@ -14,10 +20,7 @@ module.exports = [
             "background": "./app/background"
         },
         mode: "development",
-        output: {
-            filename: "[name].js",
-            path: path.join(__dirname, "app/dist")
-        },
+        output: outputLocation,
 
         // Enable sourcemaps for debugging webpack's output.
         devtool: "source-map",
@@ -37,6 +40,40 @@ module.exports = [
         plugins: [
             new webpack.DefinePlugin({
                 'API_ROOT': JSON.stringify(API_ROOT)
+            })
+        ]
+    },
+    {
+        entry: {
+            'popup': './app/popup/index.tsx'
+        },
+        mode: "development",
+        output: outputLocation,
+
+        // Enable sourcemaps for debugging webpack's output.
+        devtool: "source-map",
+
+        resolve: {
+            // Add '.ts' and '.tsx' as resolvable extensions.
+            extensions: [".ts", ".js"]
+        },
+
+        module: {
+            rules: [
+                // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+                { test: /\.tsx?$/, loader: "ts-loader" }
+            ],
+        },
+
+        plugins: [
+            new webpack.DefinePlugin({
+                'API_ROOT': JSON.stringify(API_ROOT)
+            }),
+
+            new HtmlWebpackPlugin({
+                hash: true,
+                template: './app/popup/index.html',
+                filename: 'index.html'
             })
         ]
     }
