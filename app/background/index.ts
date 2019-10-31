cc8e5369fa943645fd5436842e78298b85f82b0a
Yuto Otaguro <yotagur@gmail.com>
2019-10-30 19:31:44 -0500

    Extension only shows itself on vrv

--- app/background/index.ts
+++ app/background/index.ts
@@ -1,20 +1,24 @@
 import { IChromeMessage, ChromeMessageType } from "../classes/chrome-message";
 import { browser } from 'webextension-polyfill-ts';
-browser.tabs.onUpdated.addListener((tabId, changeInfo) => {
-    if (changeInfo.status === "complete") {
-        const message: IChromeMessage = {
-            type: ChromeMessageType.GET_VIDEO_DATA
-        };
-        
-        browser.tabs.query({ active: true }).then(tabs => {
-            tabs = tabs.filter(tab => tab.url.indexOf("vrv.co/watch") > 0);
 
-            if (tabs.length > 0) {
 
-                browser.tabs.sendMessage(tabs[0].id, message).then(response => {
-                    console.log(response);
-                });
-            }
-        });
+browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (!changeInfo.status) return;
+
+    if (changeInfo.status === "complete" && tab.url.match("vrv.co")) {
+        browser.pageAction.show(tabId);
+    } else {
+        browser.pageAction.hide(tabId);
     }
+
+    // browser.tabs.query({ active: true }).then(tabs => {
+    //     tabs = tabs.filter(tab => tab.url.indexOf("vrv.co/watch") > 0);
+
+    //     if (tabs.length > 0) {
+
+    //         browser.tabs.sendMessage(tabs[0].id, message).then(response => {
+    //             console.log(response);
+    //         });
+    //     }
+    // });
 });
