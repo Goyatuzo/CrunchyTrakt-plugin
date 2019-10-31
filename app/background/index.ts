4b089ea332270207505c530f1a2bca7b9a4afa65
Yuto Otaguro <yotagur@gmail.com>
2019-10-30 19:57:42 -0500

    Clean up unused code

--- app/background/index.ts
+++ app/background/index.ts
@@ -1,6 +1,4 @@
-import { IChromeMessage, ChromeMessageType } from "../classes/chrome-message";
 import { browser } from 'webextension-polyfill-ts';
-
 
 browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (!changeInfo.status) return;
@@ -10,15 +8,4 @@ browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     } else {
         browser.pageAction.hide(tabId);
     }
-
-    // browser.tabs.query({ active: true }).then(tabs => {
-    //     tabs = tabs.filter(tab => tab.url.indexOf("vrv.co/watch") > 0);
-
-    //     if (tabs.length > 0) {
-
-    //         browser.tabs.sendMessage(tabs[0].id, message).then(response => {
-    //             console.log(response);
-    //         });
-    //     }
-    // });
 });
