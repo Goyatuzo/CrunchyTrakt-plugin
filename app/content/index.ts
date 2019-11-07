d2a93eb7c4d807cabbf2b54933c6ebcac8ad1ba6
Yuto Otaguro <yotagur@gmail.com>
2019-11-06 18:34:24 -0600

    Remove unnecessary console logs

--- app/content/index.ts
+++ app/content/index.ts
@@ -2,15 +2,12 @@ import Vrv from "../websites/vrv";
 import { IAppMessage, AppMessageType } from "../classes/app-message";
 import { browser } from 'webextension-polyfill-ts';
 
-console.log("Content script INIT");
-
 browser.runtime.onMessage.addListener((req: IAppMessage) => {
     return new Promise((resolve, reject) => {
         switch (req.type) {
             case AppMessageType.GET_VIDEO_DATA: {
                 if (req.payload === 'vrv') {
                     const vrv = new Vrv();
-                    console.log(vrv.seriesName);
                     resolve(vrv.seriesName);
                 } else {
                     reject(`Video data for ${req.payload} was requested`);
