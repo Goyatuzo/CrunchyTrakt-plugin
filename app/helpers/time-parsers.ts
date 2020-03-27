25c12f6a5d54cc19ce3a251b0763b083e552e0d1
Yuto Otaguro <yotagur@gmail.com>
2020-03-27 01:23:03 -0500

    Remove unnecessary files

--- app/helpers/time-parsers.ts
+++
@@ -1,7 +0,0 @@
-export function mmHHToSeconds(input: string) {
-    input = input.trim();
-
-    const tokens = input.split(":").map(Number);
-    // tokens[0] is minutes and tokens[1] is seconds.
-    return tokens[0] * 60 + tokens[1];
-}
