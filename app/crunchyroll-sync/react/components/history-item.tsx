1c8035a4d16b8a96dd0e433b3908e6179ef29939
Yuto Otaguro <yotagur@gmail.com>
2020-03-28 02:09:43 -0500

    Update to show crunchyroll and trakt info

--- app/crunchyroll-sync/react/components/history-item.tsx
+++ app/crunchyroll-sync/react/components/history-item.tsx
@@ -7,10 +7,19 @@ interface IExternalProps {
 const HistoryItem: React.StatelessComponent<IExternalProps> = props => {
     return (
         <div className="card">
-            <h3 className="card-title">{props.data.media.name}</h3>
-            <img src={props.data.media.screenshot_image.thumb_url}></img>
-            <p>Season {props.data.collection.season}, Episode {props.data.media.episode_number}</p>
-            <p>{props.data.collection.complete.toString()}</p>
+            <div className="row">
+                <div className="col col-md-6">
+                    <h3 className="card-title">{props.data.media.name}</h3>
+                    <img src={props.data.media.screenshot_image.thumb_url}></img>
+                    <p>Season {props.data.collection.season}, Episode {props.data.media.episode_number}</p>
+                </div>
+
+                <div className="col col-md-6">
+                    <h3 className="card-title">{props.data.media.name}</h3>
+                    <img src={props.data.media.screenshot_image.thumb_url}></img>
+                    <p>Season {props.data.collection.season}, Episode {props.data.media.episode_number}</p>
+                </div>
+            </div>
         </div>
     )
 }
