5825f435c78ced24cb4c7df56eddc72d92ab0af1
Yuto Otaguro <yotagur@gmail.com>
2020-03-28 09:54:44 -0500

    Create case for when a search is made

--- app/crunchyroll-sync/redux/reducers/trakt.ts
+++ app/crunchyroll-sync/redux/reducers/trakt.ts
@@ -1,3 +1,24 @@
+import { IAction } from "../../../global/action";
+import { ActionType } from "../../../global/actiontype";
+
 export interface TraktState {
-    
+    searches: { [key: string]: Trakt.SearchResult };
+    isRequesting: { [key: string]: boolean }
+}
+
+const defaultState: TraktState = {
+    searches: {},
+    isRequesting: {}
+}
+
+export function reducer(state = defaultState, action: IAction) {
+    switch (action.type) {
+        case ActionType.REQUEST_TRAKT_SEARCH:
+            let currentRequests = { ...state.isRequesting };
+            currentRequests[action.value] = true;
+
+            return { ...state, requesting: currentRequests };
+        default:
+            return state;
+    }
 }
