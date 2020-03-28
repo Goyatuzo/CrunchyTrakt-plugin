50e0e1090da8bfdb74884dddfe8d33c580e6483a
Yuto Otaguro <yotagur@gmail.com>
2020-03-28 01:15:47 -0500

    Update the history interface

--- app/classes/crunchyroll.ts
+++ app/classes/crunchyroll.ts
@@ -1,5 +1,45 @@
 namespace Crunchyroll {
     export interface HistoryItem {
-        title: string;
+        playhead: string;
+        timestamp: string;
+        media: HistoryItemMedia;
+        collection: HistoryItemCollection;
+    }
+
+    interface HistoryItemMedia {
+        /**
+         * Title of the episode.
+         */
+        name: string;
+        episode_number: string;
+        /**
+         * Timestamp in a string format.
+         */
+        created: string;
+        url: string;
+        /**
+         * A bunch of URLs for images.
+         */
+        screenshot_image: {
+            thumb_url: string;
+        }
+    }
+
+    interface HistoryItemCollection {
+        class: string;
+        collection_id: string;
+        etp_guid: string;
+        series_id: string;
+        series_etp_guid: string;
+        /**
+         * Name of the anime.
+         */
+        name: string;
+        description: string;
+        season: string;
+        /**
+         * Whether or not the user has completed watching the show.
+         */
+        complete: boolean;
     }
 }
