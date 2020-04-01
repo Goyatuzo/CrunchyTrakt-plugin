ce49cb601839f07e3af53b1a27c9fb6a4eac4b87
Yuto Otaguro <yotagur@gmail.com>
2020-04-01 18:48:27 -0500

    Connect to redux

--- app/crunchyroll-sync/react/components/sync-episode-toggle.tsx
+++ app/crunchyroll-sync/react/components/sync-episode-toggle.tsx
@@ -1,19 +1,31 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import { CombinedState } from '../../redux/reducers';
 
 interface ExternalProps {
     crunchyData: Crunchyroll.HistoryItem;
     syncable: boolean;
 }
 
+interface StateToProps {
+    alreadySynced: boolean;
+}
+
 type SyncEpisodeToggleProps = ExternalProps
 
-const SyncEpisodeToggle: React.StatelessComponent<SyncEpisodeToggleProps> = props => {
+const SyncEpisodeToggleComp: React.StatelessComponent<SyncEpisodeToggleProps> = props => {
     return (
         <div className='ui toggle checkbox'>
-            <input type="checkbox" name="newsletter" disabled={!props.syncable}/>
+            <input type="checkbox" name="newsletter" disabled={!props.syncable} />
             <label>Sync to Trakt</label>
         </div>
     )
 }
+
+const SyncEpisodeToggle = connect<StateToProps, {}, ExternalProps, CombinedState>((state, ext) => {
+    return {
+        alreadySynced: state.trakt.historicScrobbles[ext.crunchyData.timestamp] !== undefined
+    }
+})(SyncEpisodeToggleComp);
 
 export default SyncEpisodeToggle;
